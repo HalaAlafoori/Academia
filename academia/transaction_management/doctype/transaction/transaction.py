ae0d362a694b7002d3b4da67f515c5041af861c1
ZGalsaqqaf <zrz10302021glg@gmail.com>
2024-05-29 20:17:11 +0300

    refact(Transaction Management): Add some validation on the requirements in Transaction doctype
    - add (Requirement) check box
    - add (Category Parent)

--- academia/transaction_management/doctype/transaction/transaction.py
+++ academia/transaction_management/doctype/transaction/transaction.py
@@ -1,63 +1,53 @@
 # Copyright (c) 2024, SanU and contributors
 # For license information, please see license.txt
 
-import json
 import frappe
 from frappe.model.document import Document
 
 class Transaction(Document):
-    # begin: auto-generated types
-    # This code is auto-generated. Do not modify anything in this block.
-
-    from typing import TYPE_CHECKING
-
-    if TYPE_CHECKING:
-        from academia.transaction_management.doctype.transaction_action.transaction_action import TransactionAction
-        from academia.transaction_management.doctype.transaction_attachments.transaction_attachments import TransactionAttachments
-        from frappe.types import DF
-
-        actions: DF.Table[TransactionAction]
-        amended_from: DF.Link | None
-        attachments: DF.Table[TransactionAttachments]
-        category: DF.Link | None
-        client: DF.Link | None
-        created_by: DF.Link | None
-        description: DF.TextEditor | None
-        full_electronic: DF.Check
-        priority: DF.Literal["", "Low", "Medium", "High", "Urgent"]
-        reference_number: DF.Data | None
-        start_date: DF.Datetime | None
-        status: DF.Literal["Pending", "Approved", "Rejected"]
-        title: DF.Data | None
-    # end: auto-generated types
     def before_save(self):
         created_by = frappe.get_doc('User', frappe.session.user)
         self.created_by = created_by.name
+    
+
+    # except frappe.DoesNotExistError as e:
+    #     return {
+    #         "error": f"Error: {e}"
+    #     }
+
+# your_module.py
+
+# get User of employee in a company an have a designation
+# @frappe.whitelist()
+# def get_user_from_designation(company, designation):
+#     # Get the employee with the given designation
+#     employee = frappe.get_all("Employee", filters={
+#         "designation": designation,
+#         "company": company
+#     }, limit=1)
+
+#     if employee:
+#         # Return the user associated with the employee
+#         return employee[0].user_id
+#     else:
+#         return None
+
+
+# @frappe.whitelist()
+# def get_transaction_actions(transaction):
+#     transaction_actions =  frappe.get_all(
+#             'Transaction Action',
+#             filters={'main_transaction': transaction},
+#             fields=['*'],
+#             order_by='creation'
+#         )
+#     return transaction_actions
   
-@frappe.whitelist()
-def get_transaction_category_requirement(transaction_category):
-    requirements = frappe.get_all("Transaction Category Requirement",
-                                   filters={"parent": transaction_category},)
-    return requirements
-
-@frappe.whitelist()
-def update_share_permissions(docname, user, permissions):
-    share = frappe.get_all("DocShare", filters={
-        "share_doctype": "Transaction",
-        "share_name": docname,
-        "user": user
-    })
-
-    permissions_dict = json.loads(permissions)
-
-    if share:
-        # Share entry exists, update the permissions
-        share = frappe.get_doc("DocShare", share[0].name)
-        share.update(permissions_dict)
-        share.save(ignore_permissions=True)
-        frappe.db.commit()
-
-    return share
+# @frappe.whitelist()
+# def get_transaction_category_requirement(transaction_category):
+#     requirements = frappe.get_all("Transaction Category  Requirement",
+#                                    filters={"parent": transaction_category},)
+#     return requirements
 
 @frappe.whitelist()
 def get_transaction_category_requirement(transaction_category):
@@ -66,8 +56,7 @@ def get_transaction_category_requirement(transaction_category):
     # Fetch requirements for the selected transaction category
     transaction_category_requirements = frappe.get_all("Transaction Category  Requirement",
                                                       filters={"parent": transaction_category},
-                                                      # fields=["name", "file_type", "required"] # causes a problem because of uniqe name 
-                                                      )
+                                                      fields=["name", "file_type", "required"])
     requirements.extend(transaction_category_requirements)
 
     # Check if the transaction category has a parent category
@@ -75,9 +64,92 @@ def get_transaction_category_requirement(transaction_category):
     if parent_category:
         # Fetch requirements for the parent category
         parent_category_requirements = frappe.get_all("Transaction Category  Requirement",
-                                                    filters={"parent": parent_category},
-                                                    # fields=["name", "file_type", "required"] # causes a problem because of uniqe name 
-                                                    )
+                                                     filters={"parent": parent_category},
+                                                      fields=["name", "file_type", "required"])
         requirements.extend(parent_category_requirements)
 
     return requirements
+
+# @frappe.whitelist()
+# def get_last_transaction_action_type(transaction):
+#     # Last_transaction_action = frappe.get_last_doc('Transaction Action',
+#     #                               filters={'main_transaction': transaction})
+#     # if Last_transaction_action:
+#     #     return Last_transaction_action.status
+#     # else:
+#     #     return ''
+#     last_transaction_action = frappe.get_list(
+#         'Transaction Action',
+#         filters={'main_transaction': transaction},
+#         fields=['type'],
+#         order_by='creation desc',
+#         limit=1
+#     )
+
+#     if last_transaction_action:
+#         return last_transaction_action[0].type
+#     else:
+#         return ''
+    
+# @frappe.whitelist()
+# def render_vertical_path(transaction):
+#         # Assuming your template file is named "vertical_path.html"
+#         # Get the directory path of the Pytho   n file
+       
+#         # template_path="/home/xair/frappe-bench/apps/academia/academia/templates/vertical_path.html"
+#         # # Load the template file
+#         # with open(template_path, "r") as file:
+#         #     template_content = file.read()
+
+#         # # Create a Jinja template object
+#         # template = Template(template_content)
+
+#         # # Fetch data from the "Transaction Action" doctype??
+#         # transaction_actions = frappe.get_all("Transaction Action", filters={}, fields=["redirected_to"])
+        
+
+#         # # Define the context variables with the fetched data
+#         # context = {
+#         #     "steps": transaction_actions
+#         # }
+        
+#         # # Render the template
+#         # rendered_html = template.render(context)
+#         document = frappe.get_doc("Transaction", transaction)
+#         #frappe.msgprint(document)
+#         # Assign the rendered HTML to the HTML field in the document
+#         #document.summary.options = rendered_html
+    
+#     # Update the options of the HTML field
+#         document.set("summary", "hello")
+#         document.set("summary_section", 0)
+#         # document.summary_section.hidden = 0
+
+#     # Save the changes
+#         document.save()
+#         return document
+
+
+# def get_permission_query(doctype, meta, role_permissions, for_list_view=False):
+#     if frappe.session.user != "Administrator":
+#         # Get the transaction IDs that the current user has access to
+#         transaction_ids = frappe.db.get_all(
+#             "Transaction Action",
+#             filters={
+#                 "redirected_to": frappe.session.user
+#             },
+#             pluck="main_transaction"
+#         )
+
+#         # Construct the query based on whether it's for the list view or not
+#         if for_list_view:
+#             return """(`tabTransaction`.`created_by` = '{0}' OR `tabTransaction`.`name` IN ({1}))""".format(
+#                 frappe.session.user,
+#                 ", ".join([f"'{tid}'" for tid in transaction_ids])
+#             )
+#         else:
+#             return """(`tabTransaction`.`name` IN ({0}))""".format(
+#                 ", ".join([f"'{tid}'" for tid in transaction_ids])
+#             )
+#     else:
+#         return None
--- academia/transaction_management/doctype/transaction_category/transaction_category.py
+++ academia/transaction_management/doctype/transaction_category/transaction_category.py
@@ -22,6 +22,16 @@ class TransactionCategory(Document):
 	# end: auto-generated types
 	pass
 
+
+# I used the one in transaction.py
+# @frappe.whitelist()
+# def get_transaction_category_requirement(transaction_category):
+#     requirements = frappe.get_all("Transaction Category  Requirement",
+#                                    filters={"parent": transaction_category},
+#                                    fields=["name", "file_type", "required"])
+#     return requirements
+
+
     	
         	
             	
